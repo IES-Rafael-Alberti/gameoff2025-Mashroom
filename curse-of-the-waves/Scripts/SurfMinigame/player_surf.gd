5cbee08a89c9571201d8d6e40d2755e0604fcf1f
EnderLz <davidsonilopez25@gmail.com>
2025-11-07 10:14:07 +0100

    feat: fused the menu with the surf minigame

--- curse-of-the-waves/Scripts/SurfMinigame/player_surf.gd
+++ curse-of-the-waves/Scripts/SurfMinigame/player_surf.gd
@@ -28,5 +28,6 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		restart_game()
 		
 func restart_game():
+	hide()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().reload_current_scene()
