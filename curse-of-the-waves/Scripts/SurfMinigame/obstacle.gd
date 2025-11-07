93ba0ac41048aa5d4f7a55d72118d9685f904d36
EnderLz <davidsonilopez25@gmail.com>
2025-11-07 11:50:37 +0100

    feat: ZIndex of objects in surf depends on height and made easier to modify movement
    ZIndex of player/objects changes between 1/10 depending on how high/low they are
    
    Changed movement borders into Markers2D

--- curse-of-the-waves/Scripts/SurfMinigame/obstacle.gd
+++ curse-of-the-waves/Scripts/SurfMinigame/obstacle.gd
@@ -3,9 +3,18 @@ extends Area2D
 
 @export var speed: float = 200.0  # Velocidad a la que se mueve el obstáculo
 
+var top_pos: float
+var bottom_pos: float
+
 func _process(delta):
 	position.x -= speed * delta  # Para que se mueva el obstáculo hacia la izquierda
 
 	# Si sale fuera de pantalla, borramos el obstáculo
 	if position.x < -100:
 		queue_free()
+		
+	zIndexHeight()
+
+func zIndexHeight():
+	var new_zIndex = (position.y-top_pos) / (bottom_pos - top_pos) * 10
+	$Sprite2D.z_index = new_zIndex + 1
--- curse-of-the-waves/Scripts/SurfMinigame/player_surf.gd
+++ curse-of-the-waves/Scripts/SurfMinigame/player_surf.gd
@@ -2,8 +2,15 @@ extends CharacterBody2D
 
 
 @export var speed: float = 200.0
-@export var limit_top: float = 350.0
-@export var limit_bottom: float = 435.0
+@export var limit_top: Marker2D
+@export var limit_bottom: Marker2D
+
+var top_pos: float
+var bottom_pos: float
+
+func _ready():
+	top_pos = limit_top.position.y
+	bottom_pos = limit_bottom.position.y 
 
 func _physics_process(delta):
 	velocity.y = 0  # Reinicia el movimiento vertical cada frame
@@ -18,11 +25,14 @@ func _physics_process(delta):
 	move_and_slide()
 
 	# Limita la posición dentro del área jugable
-	if position.y < limit_top:
-		position.y = limit_top
-	elif position.y > limit_bottom:
-		position.y = limit_bottom
+	if position.y < top_pos:
+		position.y = top_pos
+	elif position.y > bottom_pos:
+		position.y = bottom_pos
 		
+	zIndexHeight()
+	
+
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("obstacle"):
 		restart_game()
@@ -31,3 +41,7 @@ func restart_game():
 	hide()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().reload_current_scene()
+
+func zIndexHeight():
+	var new_zIndex = (position.y-top_pos) / (bottom_pos - top_pos) * 10
+	$Sprite2D.z_index = new_zIndex + 1
