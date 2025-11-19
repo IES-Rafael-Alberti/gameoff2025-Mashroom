9d89f5bfd5d5bee044d034ffacaa13e67f8403c7
Aki <akari24yowane@gmail.com>
2025-11-19 10:55:20 +0100

    fix: cambio audio

--- curse-of-the-waves/Scripts/Menu/menu.gd
+++ curse-of-the-waves/Scripts/Menu/menu.gd
@@ -14,6 +14,12 @@ func _on_play_button_pressed() -> void:
 	$OptionsButton.focus_mode  = Control.FOCUS_NONE
 	var GameManager = get_tree().get_root().get_node("Main/GameManager")
 	%GameManager.loadSceneDialogic(preload("res://Scenes/SurfMinigame/SurfMinigame.tscn"), '1-prologue')
+	AudioPlayer.stopMusic()
+	AudioPlayer.pulsarBtn()
+	Dialogic.start('1-prologue')
+	await Dialogic.timeline_ended
+	get_tree().change_scene_to_file("res://Scenes/SurfMinigame/SurfMinigame.tscn")
+	#AudioPlayer.musicMinijuegoSurf()
 	
 
 func _on_options_button_pressed():
