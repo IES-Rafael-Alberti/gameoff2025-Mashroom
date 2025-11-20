5898ccd1ac99b62af8b7d03be326be09e4acdeac
Aki <akari24yowane@gmail.com>
2025-11-20 10:42:06 +0100

    fix: cambio audio

--- curse-of-the-waves/Scripts/SurfMinigame/surfplayer.gd
+++ curse-of-the-waves/Scripts/SurfMinigame/surfplayer.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var Hp = 3
 @export var BaseSpeed = 800.0
 
-@onready var sonidoCaida = preload("res://assets/Audio/SFX/splash_effect.wav")
+@onready var sonidoCaida = preload("res://assets/Audio/SFX/Minijuego surf/efectoSplash.wav")
 @onready var anims = $Sprite2D
 @onready var heart_textures = {
 	"full": preload("res://assets/GUI/Heart.png"),
@@ -69,7 +69,7 @@ func death(): # Proceso de Muerte
 	canMove = false
 	canBeDamaged = false
 	anims.play("death")
-	await get_tree().create_timer(0.999).timeout #para sfx de caida y que espere
+	await get_tree().create_timer(0.998).timeout #para sfx de caida y que espere
 	AudioPlayer.playSfx(sonidoCaida, -12.0)
 	await anims.animation_finished
 	# Temporal, para testeo
