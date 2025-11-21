8ac2ca51b5749a14307346d2c878aa3f888defdd
EnderLz <davidsonilopez25@gmail.com>
2025-11-21 12:30:22 +0100

    feat: added simple control changes

--- curse-of-the-waves/Scripts/Menu/OptionsMenu.gd
+++ curse-of-the-waves/Scripts/Menu/OptionsMenu.gd
@@ -11,7 +11,7 @@ func showOptionMenu(show: bool):
 	if show:
 		show()
 		OptionAnim.play("OptionsMenu")
-		$OptionsBackground/Control/BackButton.grab_focus()
+		$OptionsBackground/Options/BackButton.grab_focus()
 	else:
 		OptionAnim.play_backwards("OptionsMenu")
 		await OptionAnim.animation_finished
--- curse-of-the-waves/Scripts/Menu/menu.gd
+++ curse-of-the-waves/Scripts/Menu/menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 @export_group("Objects")
 @export var OptionsContainer: Control
+@export var Controls: HBoxContainer
+
+var changeControl = ""
+var changeButton: Button
 
 func _ready():
 	AudioPlayer.musicNivel()
@@ -31,3 +35,50 @@ func _on_back_button_pressed():
 func _on_option_button_item_selected(index):
 	var GameManager = get_tree().get_root().get_node("Main/GameManager")
 	GameManager.changeLanguage(index)
+	
+func _input(event):
+	if event is InputEventKey and changeControl != "":
+		InputMap.action_erase_events(changeControl)
+		InputMap.action_add_event(changeControl, event)
+		updateButton(changeButton, changeControl)
+	
+func updateButton(button: Button, movement: String):
+	var events = InputMap.action_get_events(movement)
+	if events.size() > 0:
+		button.text = events[0].as_text()
+	else: button.text = ""
+	button.button_pressed = false
+
+func selectChange(button: Button, toggle: bool, movement: String):
+	if toggle:
+		changeControl = movement
+		changeButton = button
+		button.text = "ui_pressKey"
+	else:
+		changeButton = null
+		changeControl = ""
+		updateButton(button, movement)
+
+func _on_up_button_toggled(toggled_on):
+	var button = Controls.get_node("MovementVBox/UpHBox/UpButton")
+	selectChange(button, toggled_on, "move_up")
+
+func _on_down_button_toggled(toggled_on):
+	var button = Controls.get_node("MovementVBox/DownHBox/DownButton")
+	selectChange(button, toggled_on, "move_down")
+
+func _on_left_button_toggled(toggled_on):
+	var button = Controls.get_node("MovementVBox/LeftHBox/LeftButton")
+	selectChange(button, toggled_on, "move_left")
+
+func _on_right_button_toggled(toggled_on):
+	var button = Controls.get_node("MovementVBox/RightHBox/RightButton")
+	selectChange(button, toggled_on, "move_right")
+
+func _on_main_button_toggled(toggled_on):
+	var button = Controls.get_node("ActionVBox/MainHBox/MainButton")
+	selectChange(button, toggled_on, "MainAction")
+
+func _on_second_button_toggled(toggled_on):
+	var button = Controls.get_node("ActionVBox/SecondaryHBox/SecondButton")
+	selectChange(button, toggled_on, "SecondaryAction")
