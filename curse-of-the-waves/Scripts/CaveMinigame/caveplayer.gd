ad5eeff3ab438fa0039195621799a61508294679
Aki <akari24yowane@gmail.com>
2025-11-28 10:34:38 +0100

    fix: arreglos y añadido de audio de tormenta para el minijuego de surf

--- curse-of-the-waves/Scripts/CaveMinigame/caveplayer.gd
+++ curse-of-the-waves/Scripts/CaveMinigame/caveplayer.gd
@@ -99,7 +99,7 @@ func _on_hitbox_area_entered(area):
 func _finish():
 	var game_manager = get_tree().get_root().get_node("Main/GameManager")
 	AudioPlayer.stop_music()
-	game_manager.load_scene_dialogic(preload("res://Scenes/FinishGame.tscn"), '3-cave_scene')
+	#game_manager.load_scene_dialogic(preload("res://Scenes/FinishGame.tscn"), '3-cave_scene')
 
 
 func _take_damage():
