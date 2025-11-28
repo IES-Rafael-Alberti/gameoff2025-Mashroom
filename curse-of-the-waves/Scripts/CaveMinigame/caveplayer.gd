beab8a85410faa0af2c10104ab161acee3b52c53
Aki <akari24yowane@gmail.com>
2025-11-28 08:04:09 +0100

    fix: arreglo audio despues del minijuego de cueva

--- curse-of-the-waves/Scripts/CaveMinigame/caveplayer.gd
+++ curse-of-the-waves/Scripts/CaveMinigame/caveplayer.gd
@@ -96,6 +96,7 @@ func _on_hitbox_area_entered(area):
 
 func _finish():
 	var game_manager = get_tree().get_root().get_node("Main/GameManager")
+	AudioPlayer.stop_music()
 	game_manager.load_scene_dialogic(preload("res://Scenes/FinishGame.tscn"), '3-cave_scene')
 
 
